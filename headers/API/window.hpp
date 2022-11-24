507ba0690977eaeb62a03bf6b219bf1683023b3f
b0ryahha <tosha.mar4encko2015@yandex.ru>
2022-11-24 20:53:45 +0700

    Boosted window api.

--- headers/API/window.hpp
+++ headers/API/window.hpp
@@ -36,14 +36,76 @@ namespace lua
     }
 
     static int get_size(lua_State* L) {
-        LuaStack args(L);
-
         lua_pushtable(L, {
             std::make_pair("w", window.getSize().x),
             std::make_pair("h", window.getSize().y)
         });
 
         return 1;
+    }
+
+    static int set_size(lua_State* L) {
+        LuaStack args(L);
+
+        if (args.size() != 2)
+            throw_error("Incorrect number of arguments!");
+
+        window.setSize(sf::Vector2u(args.get<size_t>(0), args.get<size_t>(1)));
+
+        return 0;
+    }
+
+    static int get_pos(lua_State* L) {
+        lua_pushtable(L, {
+            std::make_pair("x", window.getPosition().x),
+            std::make_pair("y", window.getPosition().y)
+        });
+
+        return 1;
+    }
+
+    static int set_pos(lua_State* L) {
+        LuaStack args(L);
+
+        if (args.size() != 2)
+            throw_error("Incorrect number of arguments!");
+
+        window.setPosition(sf::Vector2i(args.get<int>(0), args.get<int>(1)));
+
+        return 0;
+    }
+
+    static int set_title(lua_State* L) {
+        LuaStack args(L);
+
+        if (args.size() != 1)
+            throw_error("Incorrect number of arguments!");
+
+        window.setTitle(args.get<std::string>());
+
+        return 0;
+    }
+
+    static int set_vsync(lua_State* L) {
+        LuaStack args(L);
+
+        if (args.size() != 1)
+            throw_error("Incorrect number of arguments!");
+
+        window.setVerticalSyncEnabled(args.get<bool>());
+
+        return 0;
+    }
+
+    static int set_frame_limit(lua_State* L) {
+        LuaStack args(L);
+
+        if (args.size() != 1)
+            throw_error("Incorrect number of arguments!");
+
+        window.setFramerateLimit(args.get<size_t>());
+
+        return 0;
     }
 
     static int sleep(lua_State* L) {
